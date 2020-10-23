b944899dd98e56f19a7d2b23f20be5e3a6f982b1
seledtsov669 <aleksandrseledcov669@gmail.com>
2020-10-24 01:02:06 +0700

    Funcation rectangle

--- main.py
+++ main.py
@@ -29,3 +29,36 @@ def square(x, y, side, angle, color):
     turtle.right(90)
     turtle.end_fill()
 
+
+
+def rectangle(x, y, width, length, angle, color):
+    '''
+    Function, drawing rectangle
+    :param x: upper left corner coordinate X
+    :param y: upper left corner coordinate Y
+    :param width: side width of a rectangle
+    :param length: side length of a rectangle
+    :param angle: the mounting angle of a rectangle
+    :param color: color of a rectangle frame and fill
+    :return: None
+    ToDo: Seledtsov
+    '''
+    turtle.home()
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.color(color, color)
+
+    turtle.down()
+    turtle.begin_fill()
+    turtle.right(angle)
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.end_fill()
+
+
