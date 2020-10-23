c61474ec92188f7c332878a5169552609a844fea
oleg <marinkin.slav@mail.ru>
2020-10-24 00:55:02 +0700

    add triangle 60 degrees by Oleg

--- main.py
+++ main.py
@@ -23,3 +23,32 @@ def trapeze(x, y, side, angle, color):
     turtle.forward(side)
     turtle.end_fill()
     turtle.up()
+
+
+def triangle60(x, y, side, angle, color):
+    '''
+    Function, drawing equilateral triangle
+    :param x: upper left corner coordinate X
+    :param y: upper left corner coordinate Y
+    :param side: side length of a triangle
+    :param angle: the mounting angle of a triangle
+    :param color: color of a triangle frame and fill
+    :return: None
+    ToDo: Marinkin
+    '''
+    turtle.home()
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.color(color, color)
+
+    turtle.down()
+    turtle.begin_fill()
+    turtle.right(angle)
+    turtle.forward(side)
+    turtle.left(120)
+    turtle.forward(side)
+    turtle.left(120)
+    turtle.forward(side)
+    turtle.end_fill()
+    turtle.up()
+
