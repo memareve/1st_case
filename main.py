e729880e97a9c37b748c87607331bf0d16443435
memareve <yqqN87cjbeER>
2020-10-24 20:46:17 +0700

    fix all syntax mistakes and color correction

--- main.py
+++ main.py
@@ -81,7 +81,8 @@ def hexagon(x, y, radius, angle, color):
     turtle.circle(radius, 360, 6)
     turtle.end_fill()
     turtle.up()
-    
+
+
 def trapeze(x, y, side, angle, color):
     '''
     Function, drawing trapeze
@@ -268,17 +269,14 @@ def rocket():
     :return: None
     ToDo: Seledtsov
     '''
-    turtle.shape('turtle')
-    turtle.speed(10)
-    triangle60(-830, -430, 140, 270, 'tomato')
-    rectangle(-828, -130, 200, 300, 0, 'darkgrey')
-    triangle60(-626, -290, 140, 90, 'tomato')
-    triangle60(-828, -128, 200, 0, 'tomato')
-    circle(-750, -200, 30, 300, 'white')
-    circle(-750, -300, 30, 300, 'white')
-    circle(-750, -400, 30, 300, 'white')
-    trapeze(-655, -492, 70, 240, 'orange')
-    turtle.home()
+    triangle60(-830, -430, 140, 270, '#4ab19c')
+    rectangle(-828, -130, 200, 300, 0, '#374d5b')
+    triangle60(-626, -290, 140, 90, '#2a6779')
+    triangle60(-828, -128, 200, 0, '#de584d')
+    circle(-750, -200, 30, 300, '#b5c3c6')
+    circle(-750, -300, 30, 300, '#b5c3c6')
+    circle(-750, -400, 30, 300, '#b5c3c6')
+    trapeze(-655, -492, 70, 240, '#fb9728')
     pass
 
 
@@ -288,51 +286,43 @@ def rabbit():
     :return: None
     ToDo: Seledtsov
     '''
-    turtle.shape('turtle')
-    turtle.speed(10)
-    triangle90(-828, 120, 100, 0, 'dodgerblue')
-    triangle90(-726, 195, 75, 90, 'purple')
-    triangle90(-691, 260, 140, 180, 'yellow')
-    triangle90(-831, 262, 140, 0, 'red')
-    triangle60(-688, 297, 70, 90, 'magenta')
-    square(-688, 437, 70, 0, 'orange')
-    parallelogram(-653, 439, 70, 70, 240, 'lime')
-    turtle.home()
+    triangle90(-828, 120, 100, 0, '#374d5b')
+    triangle90(-726, 195, 75, 90, '#de584d')
+    triangle90(-691, 260, 140, 180, '#feb836')
+    triangle90(-831, 262, 140, 0, '#4ab19c')
+    triangle60(-688, 297, 70, 90, '#b5c3c6')
+    square(-688, 437, 70, 0, '#2a6779')
+    parallelogram(-653, 439, 70, 70, 240, '#fb9728')
     pass
   
-  
-  def robot():
+
+def robot():
     '''
     Function, drawing robot
     :return: None
     ToDo: Marinkin
     '''
-    turtle.speed(10)
-    triangle60(-40, -500, 20, 0, 'blue')
-    triangle60(40,  -500, 20, 0, 'blue')
-    rectangle(-38, -415, 15, 70, 0, 'red')
-    turtle.up()
-    rectangle(42, -415, 15, 70, 0, 'red')
-    turtle.up()
-    square(-65, -270,150,0,"blue")
-    turtle.up()
-    rectangle(-137, -260, 290, 10, 0, 'red')
-    turtle.up()
-    circle(-100, -310, 20, 0, 'blue')
-    rectangle(-110, -310, 20, 50, 0, 'red')
-    circle(-100, -400, 20, 0, 'blue')
-    circle(120, -310, 20, 0, 'blue')
-    rectangle(110, -310, 20, 50, 0, 'red')
-    circle(120, -400, 20, 0, 'blue')
-    square(-15, -210, 50, 0, 'blue')
-    trapeze(49,-210, 40, 240, 'red' )
-    circle(-2, -230, 3, 0, "black")
-    circle(25, -230, 3, 0, "black")
-    square(10, -280, 8, 45, 'black')
-    square(10, -300, 8, 45, 'black')
-    square(10, -320, 8, 45, 'black')
-    square(10, -340, 8, 45, 'black')
-
+    triangle60(-40, -500, 20, 0, '#4ab19c')
+    triangle60(40,  -500, 20, 0, '#4ab19c')
+    rectangle(-38, -415, 15, 70, 0, '#fb9728')
+    rectangle(42, -415, 15, 70, 0, '#feb836')
+    square(-65, -270,150,0,"#2a6779")
+    rectangle(-137, -260, 290, 10, 0, '#374d5b')
+    circle(-100, -310, 20, 0, '#feb836')
+    rectangle(-110, -310, 20, 50, 0, '#4ab19c')
+    circle(-100, -400, 20, 0, '#fb9728')
+    circle(120, -310, 20, 0, '#fb9728')
+    rectangle(110, -310, 20, 50, 0, '#4ab19c')
+    circle(120, -400, 20, 0, '#feb836')
+    square(-15, -210, 50, 0, '#2a6779')
+    trapeze(49,-210, 40, 240, '#de584d' )
+    circle(-2, -230, 3, 0, "#374d5b")
+    circle(25, -230, 3, 0, "#374d5b")
+    square(10, -280, 8, 45, '#b5c3c6')
+    square(10, -300, 8, 45, '#b5c3c6')
+    square(10, -320, 8, 45, '#b5c3c6')
+    square(10, -340, 8, 45, '#b5c3c6')
+    pass
 
 
 def bull():
@@ -341,24 +331,22 @@ def bull():
     :return: None
     ToDo: Marinkin
     '''
-    turtle.speed(10)
-    triangle90(0, 100, 100, -45, "black")
-    triangle90(129, 112, 80, -135, 'red')
-    triangle90(146, 100, 100, -90, 'black' )
-    parallelogram(147, 190, 65, 15, 50, 'yellow')
-    square(-62, 260, 60, 0, 'red')
-    oval(-65, 280, 20, 90, 'grey')
-    oval(5, 275, 20, -25, 'orange')
-    turtle.up()
+    triangle90(0, 100, 100, -45, "#374d5b")
+    triangle90(129, 112, 80, -135, '#de584d')
+    triangle90(146, 100, 100, -90, '#2a6779' )
+    parallelogram(147, 190, 60, 20, 50, '#4ab19c')
+    square(-62, 260, 60, 0, '#fb9728')
+    oval(-65, 280, 20, 90, '#feb836')
+    oval(5, 272, 20, -17, '#b5c3c6')
+    pass
     
     
-    def sword():
+def sword():
     '''
     Function, drawing sword
     :return: None
     ToDo: Evdischenko
     '''
-    turtle.speed(5)
     trapeze(680, -20, 65, 60, '#fb9728')
     hexagon(745, -60, 15, 0, '#374d5b')
     hexagon(720, -55, 7, 0, '#2a6779')
@@ -378,7 +366,7 @@ def bull():
 
 def man_on_horse():
     '''
-    Function, drawing sword
+    Function, drawing man on horse
     :return: None
     ToDo: Evdischenko
     '''
@@ -392,3 +380,14 @@ def man_on_horse():
     trapeze(664, 384, 45, 220, '#374d5b')
     hexagon(810, 270, 5, 0, '#374d5b')
     pass
+
+
+#drawing pictures
+turtle.tracer(0)
+rocket()
+rabbit()
+robot()
+bull()
+sword()
+man_on_horse()
+turtle.mainloop()
